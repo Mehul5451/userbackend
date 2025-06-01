4f19d4f8b714215e4775fd649389751134a2b698
Mehul Gadhadara <gadhadaramehul786@gmail.com>
2025-06-01 15:16:51 +0200

    DJBOOKING

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 
 require("./conn");
 
-const { Userdata, djBooking , ticketBooking  , Event} = require("./register");
+const { Userdata, djBooking , ticketBooking  } = require("./register");
 const { Admin, Event , DJ} = require("./login");
 //const {Event} = require("../adminlogin/login");
 
